f8ed5951aff88a4d3a1c7df85d736274adb5f478
Saranchenkov <ivan.saranchenkov@gmail.com>
2017-02-23 22:54:45 +0200

    INSERT query was corrected.

--- src/main/sql/database_creation_script.sql
+++ src/main/sql/database_creation_script.sql
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS `test`.`user` (
   UNIQUE INDEX `name_UNIQUE` (`name` ASC));
 
 
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('daniil',47,1,'1996-06-10 14:07:15');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('ivan',21,1,'1996-07-07 01:01:06');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('ignat',49,1,'1996-07-03 12:01:06');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('viktor',45,0,'1996-07-03 12:01:06');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('gena',52,0,'1985-06-03 01:01:06');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('michno',19,0,'1994-12-24 12:36:09');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('james',56,0,'1985-06-03 01:01:06');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('Romzik',18,1,'1996-07-03 12:01:06');
-INSERT INTO `user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('tolik',78,0,'1994-12-24 12:34:59');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('daniil',47,1,'1996-06-10 14:07:15');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('ivan',21,1,'1996-07-07 01:01:06');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('ignat',49,1,'1996-07-03 12:01:06');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('viktor',45,0,'1996-07-03 12:01:06');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('gena',52,0,'1985-06-03 01:01:06');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('michno',19,0,'1994-12-24 12:36:09');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('james',56,0,'1985-06-03 01:01:06');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('Romzik',18,1,'1996-07-03 12:01:06');
+INSERT INTO `test`.`user` (`name`,`age`,`isAdmin`,`createdDate`) VALUES ('tolik',78,0,'1994-12-24 12:34:59');
 
