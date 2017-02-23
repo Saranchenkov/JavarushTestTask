ebd86c24cb9de6a786049b7331062949bfb68438
Saranchenkov <ivan.saranchenkov@gmail.com>
2017-02-23 22:42:35 +0200

    Database name was updated

--- src/main/sql/database_creation_script.sql
+++ src/main/sql/database_creation_script.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `javarush`.`user` (
+CREATE TABLE IF NOT EXISTS `test`.`user` (
   `id` INT(8) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(25) NOT NULL,
   `age` INT(11) NOT NULL,
